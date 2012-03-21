b8fa0f41c0710a0abc40bb75f647ec6c9c36abac
jcsleeman <its.sleeman@hotmail.co.uk>
2012-03-21 22:05:03 +0000

    Added gstreamer server and client files

---
+++ UserInterface/gstClient.h
@@ -0,0 +1,17 @@
+#ifndef GSTCLIENT_H_
+define GSTCLIENT_H_
+
+//#define STANDALONE 
+
+void killGst();
+void playGst();
+void pauseGst();
+char * getTimeGst();
+void seekGst()
+
+#ifndef STANDALONE
+int gst(int port, char ip[])
+#endif
+
+#define TRUE 1
+#define FALSE 0
